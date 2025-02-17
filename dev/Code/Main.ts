642e1e753c51f578e13d36f1dd9a0896369d4c01
CalvinDO <calvindelloro@mail.de>
2025-02-17 14:46:42 +0100

    update(main): structure, content foudn log

--- dev/Code/Main.ts
+++ dev/Code/Main.ts
@@ -1,49 +1,68 @@
 namespace Portfolio {
-    document.addEventListener("DOMContentLoaded", function () {
-        let innerSections = document.querySelectorAll<HTMLElement>('.inner'); // Holt alle Elemente mit der Klasse .inner
-        // Überprüft, ob es überhaupt Elemente gibt
-        if (innerSections.length > 0) {
-            // Schleife über jedes gefundene Element
-            innerSections.forEach(function (innerSection) {
-                // Setzt die Breite jedes Elements auf 80% oder 1200px
-                innerSection.style.setProperty('width', '85.4102%', 'important');  // Oder '1200px', je nach Bedarf
-                innerSection.style.setProperty('max-width', 'none', 'important'); // Entfernt max-width
-                innerSection.style.setProperty('min-width', 'none', 'important');  // oder '1200px', je nach Bedarf
-            });
-        }
-    });
 
-    console.log("test");
+    init();
 
-    console.log("all flex items length: " + document.querySelectorAll('.flex-item').length);
-    document.querySelectorAll('.flex-item').forEach(item => {
-        console.log("current item: " + item);
-    });
+    function init() {
 
-    const item = document.querySelector('.flex-item');
+        setWidth();
 
-    const container: HTMLDivElement = <HTMLDivElement>item.querySelector('.visual-presentation-container');
-    const content = item.querySelector('.toggle-content');
-    const arrow = item.querySelector('.toggle-arrow');
+        document.querySelectorAll('.flex-item').forEach(handleFlexItem);
 
-    let isMobile = window.matchMedia("(max-width: 768px)").matches;
-    console.log("isMobile? " + isMobile);
-    if (isMobile) {
-        arrow.addEventListener('click', () => {
-            item.classList.toggle('expanded');
-        });
-    } else {
-        container.addEventListener('mouseenter', () => {
-            console.log("mouseenter");
-            console.log("in" + container);
-            item.classList.add('expanded');
-        });
-        item.addEventListener('mouseleave', (event) => {
-            if (!item.contains(event.relatedTarget)) {
-                console.log("mouseleave");
-                console.log("in" + container);
-                item.classList.remove('expanded');
+    }
+
+    function setWidth(): void {
+        document.addEventListener("DOMContentLoaded", function () {
+            let innerSections = document.querySelectorAll<HTMLElement>('.inner'); // Holt alle Elemente mit der Klasse .inner
+            // Überprüft, ob es überhaupt Elemente gibt
+            if (innerSections.length > 0) {
+                // Schleife über jedes gefundene Element
+                innerSections.forEach(function (innerSection) {
+                    // Setzt die Breite jedes Elements auf 80% oder 1200px
+                    innerSection.style.setProperty('width', '85.4102%', 'important');  // Oder '1200px', je nach Bedarf
+                    innerSection.style.setProperty('max-width', 'none', 'important'); // Entfernt max-width
+                    innerSection.style.setProperty('min-width', 'none', 'important');  // oder '1200px', je nach Bedarf
+                });
             }
         });
+    }
+
+    function handleFlexItem(item): void {
+        const container: HTMLDivElement = <HTMLDivElement>item.querySelector('.visual-presentation-container');
+        const content = item.querySelector('.toggle-content');
+
+        if (!content) {
+            return;
+        }
+
+        console.log("content found");
+
+        const arrow = item.querySelector('.toggle-arrow');
+
+        let isMobile = window.matchMedia("(max-width: 768px)").matches;
+        console.log("isMobile? " + isMobile);
+
+        if (isMobile) {
+
+            arrow.addEventListener('click', () => {
+                item.classList.toggle('expanded');
+            });
+
+        } else {
+
+            container.addEventListener('mouseenter', () => {
+                console.log("mouseenter");
+                console.log("in" + container);
+                item.classList.add('expanded');
+            });
+
+            item.addEventListener('mouseleave', (event) => {
+                if (!item.contains(event.relatedTarget)) {
+                    console.log("mouseleave");
+                    console.log("in" + container);
+                    item.classList.remove('expanded');
+                }
+            });
+
+        }
     }
 }
