702c239664b60320b03f28aba8c742b9cf1ed05c
CalvinDO <calvindelloro@mail.de>
2025-02-28 18:39:05 +0100

    update(main): remove ,2025

--- dev/Code/Main.ts
+++ dev/Code/Main.ts
@@ -345,7 +345,8 @@ namespace Portfolio {
         let header: HTMLElement | null = document.querySelector("header");
 
         let quote: HTMLQuoteElement = document.createElement("blockquote");
-        quote.innerHTML = '<span>"Calvin Dell’Oro zählt unter den etlichen hundert Studierenden, <br>die ich seit 2008 unterrichtet habe, <br>zu den drei engagiertesten und erfolgreichsten"</span><footer>— <cite class="author"> Prof. Dr. rer. nat. Thomas Schneider</cite>, <cite class="quote-time">2025</cite></footer>'
+        quote.innerHTML = '<span>"Calvin Dell’Oro zählt unter den etlichen hundert Studierenden, <br>die ich seit 2008 unterrichtet habe, <br>zu den drei engagiertesten und erfolgreichsten"</span><footer>— <cite class="author"> Prof. Dr. rer. nat. Thomas Schneider</cite></footer>'
+        /*, <cite class="quote-time">2025</cite>*/
 
         let quoteContainer: HTMLDivElement = document.createElement("div");
         quoteContainer.classList.add("quote-container");
