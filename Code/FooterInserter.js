0243258832119323dc19340af61451daac16fbae
CalvinDO <calvindelloro@mail.de>
2025-03-08 20:44:12 +0100

    refactor(dev, beta, main)

---
+++ Code/FooterInserter.js
@@ -0,0 +1,30 @@
+var Portfolio;
+(function (Portfolio) {
+    let footer;
+    try {
+        setupFooterDocuments();
+    }
+    catch (error) {
+        console.warn(error);
+    }
+    function setupFooterDocuments() {
+        let documentsWrapper = document.querySelector(".documents-wrapper");
+        let documentsHeading = document.querySelector("#dokumente");
+        footer.appendChild(documentsHeading);
+        footer.appendChild(documentsWrapper);
+    }
+    function handleInteraction(ev) {
+        footer = document.querySelector("#footer_wrap footer");
+        if (footer) {
+            if (!footer.contains(document.querySelector(".documents-wrapper"))) {
+                setupFooterDocuments();
+            }
+        }
+    }
+    document.body.addEventListener('mousemove', handleInteraction);
+    document.body.addEventListener('scroll', handleInteraction);
+    document.body.addEventListener('keydown', handleInteraction);
+    document.body.addEventListener('click', handleInteraction);
+    document.body.addEventListener('touchstart', handleInteraction);
+})(Portfolio || (Portfolio = {}));
+//# sourceMappingURL=FooterInserter.js.map
